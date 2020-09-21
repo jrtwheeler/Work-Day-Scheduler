8c78ce71c4a643361658cb7fbad16aa647ea2d2a
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-09-20 22:46:51 -0400

    Added get hour, forEach function to text value of hour div

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,23 @@
+//establish time in a variable
+var date = moment().get('date');
+var month = moment().get('month');  
+var year = moment().get('year');
+var hour = moment().get('hour');
+var minute = moment().get('minute');
+var second = moment().get('second');
+var millisecond = moment().get('millisecond');
 
-var time = moment('01/01/2016', 'MM/DD/YYYY');
+var displayTime = moment().format("dddd, MMMM Do, YYYY");
+var rowTime = $("div.hour");
+var rowTimeHours = $("div.hour").text;
+console.log(rowTimeHours)
 //Select the class currentDay and add text;
-$("#currentDay").text(moment(time));
+$("#currentDay").text(displayTime);
+
+$('div.hour').each(function(){
+    if ($(this).text().slice(-2) < hour){
+        console.log(hour)
+    }
+  })
+
+//while (hour < )
