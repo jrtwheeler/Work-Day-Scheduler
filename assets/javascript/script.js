213691ba19cf1daccdc3f804feafe592da335ed6
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-09-22 23:12:33 -0400

    Added individual entry

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,11 @@
 //establish time in a variable
-var date = moment().get('date');
-var month = moment().get('month');  
-var year = moment().get('year');
-var hour = moment().get('hour');
-var minute = moment().get('minute');
-var second = moment().get('second');
-var millisecond = moment().get('millisecond');
+var date = moment().get("date");
+var month = moment().get("month");
+var year = moment().get("year");
+var hour = moment().get("hour");
+var minute = moment().get("minute");
+var second = moment().get("second");
+var millisecond = moment().get("millisecond");
 
 var displayTime = moment().format("dddd, MMMM Do, YYYY");
 var rowTime = $("div.hour");
@@ -13,36 +13,41 @@ var textArea = $(".description");
 
 //Select the class currentDay and add text;
 
+function plannerRowBackgrounds() {
+  var text = $(this).text();
+  var stringText = text.toString();
+  var stringTextTrimmed = stringText.trim();
+  var stringTextTrimmedSliced = stringTextTrimmed.substring(
+    0,
+    stringTextTrimmed.length - 2
+  );
+  var stringTextTrimmedSlicedNumber = parseInt(stringTextTrimmedSliced);
 
-function plannerRowBackgrounds () {
-    var text = $(this).text();
-    var stringText = text.toString();
-    var stringTextTrimmed = stringText.trim();
-    var stringTextTrimmedSliced = stringTextTrimmed.substring(0, stringTextTrimmed.length - 2);
-    var stringTextTrimmedSlicedNumber = parseInt(stringTextTrimmedSliced);
-
-    if(stringTextTrimmedSlicedNumber >= 1 && stringTextTrimmedSlicedNumber <= 5){
-        stringTextTrimmedSlicedNumber = stringTextTrimmedSlicedNumber + 12;
-    }
-
-    if (stringTextTrimmedSlicedNumber < hour){
-        textArea.addClass("bg-danger");
-    }
-    if (stringTextTrimmedSlicedNumber > hour){
-        textArea.addClass("future");
-    }
-    if (stringTextTrimmedSlicedNumber === hour){
-        textArea.addClass("present");
-    }
+  if (
+    stringTextTrimmedSlicedNumber >= 1 &&
+    stringTextTrimmedSlicedNumber <= 5
+  ) {
+    stringTextTrimmedSlicedNumber = stringTextTrimmedSlicedNumber + 12;
   }
 
-$("#currentDay").text(displayTime);
-$('.hour').each(plannerRowBackgrounds);
+  if (stringTextTrimmedSlicedNumber < hour) {
+    textArea.addClass("past");
+  }
 
-//while (hour < )
-//event.preventDefault
-//var input = (whateverdiv).val().trim
-//set that to localStorage
-//get it from localStorage
-//data attribute
-//data-hour
+  if (stringTextTrimmedSlicedNumber > hour) {
+    textArea.addClass("future");
+  }
+
+  if (stringTextTrimmedSlicedNumber === hour) {
+    textArea.addClass("present");
+  }
+}
+
+  $(".saveBtn").on("click", function () {
+    var textAreaClass = $(this).siblings('textarea');
+    localStorage.setItem("userInput", textAreaClass.val());  
+  });
+
+
+$("#currentDay").text(displayTime);
+$(".hour").each(plannerRowBackgrounds);
